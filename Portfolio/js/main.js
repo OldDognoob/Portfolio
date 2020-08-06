4c0cbf9df2e18eb914526fcdeba68dc2a97cf628
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2020-08-06 18:46:37 +0300

    corrections

--- Portfolio/js/main.js
+++ Portfolio/js/main.js
@@ -1,4 +1,4 @@
-/*===== MENU SHOW =====*/ 
+/*===== MENU =====*/ 
 const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
@@ -11,21 +11,17 @@ const showMenu = (toggleId, navId) =>{
 }
 showMenu('nav-toggle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU =====*/
 const navLink = document.querySelectorAll('.nav__link');   
 
 function linkAction(){
-  /*Active link*/
   navLink.forEach(n => n.classList.remove('active'));
   this.classList.add('active');
-  
-  /*Remove menu mobile*/
   const navMenu = document.getElementById('nav-menu')
   navMenu.classList.remove('show')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== REVEAL ANIMATION =====*/
+/*===== ANIMATION STYLE =====*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -50,4 +46,6 @@ sr.reveal('.skills__img',{delay: 600});
 sr.reveal('.work__img',{interval: 200}); 
 
 sr.reveal('.contact__input',{interval: 200}); 
+
+/*===== DARK MODE =====*/
 
